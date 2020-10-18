73474bf01b72bbb0ec11e021caf48c044c296a01
PetrNikitin <zexa91x0@mail.ru>
2020-10-18 12:42:54 +0500

    Structure refactoring

--- src/main/java/ru/guteam/cookstarter/exceptions/SearchingNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.guteam.cookstarter.exceptions;
-
-public class SearchingNotFoundException extends RuntimeException {
-    public SearchingNotFoundException(String message) {
-        super(message);
-    }
-}
