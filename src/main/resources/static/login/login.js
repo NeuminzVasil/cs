50b9332ab4000d783f04281a62268c96444deba3
Zexa91x0 <Zexa91x0@mail.ru>
2020-11-03 15:19:24 +0500

    prepare to SPB

--- src/main/resources/static/login/login.js
+++ src/main/resources/static/login/login.js
@@ -1,12 +1,13 @@
 /// <reference path = "index.js"/>
+const contextPathUserService = 'https://cookstarter-users-service.herokuapp.com'
 
 app.controller('loginControllerApiV1', function ($log, $scope, $window, $http, $localStorage) {
     $scope.tryToAuth = function () {
-        $http.post(contextPath + '/api/v1/login', $scope.user)
+        $http.post(contextPathUserService + '/auth', $scope.user)
             .then(function (response) {
                 if (response.data.token) {
                     $http.defaults.headers.common.Authorization = 'Bearer ' + response.data.token;
-                    //$log.info("birer:" , response.data.token);
+                    $log.info("birer:" , response.data.token);
                     $localStorage.currentUser = {username: $scope.user.username, token: response.data.token};
                     $window.location.href = '#!/';
                 }
